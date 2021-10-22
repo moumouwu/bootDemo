68ac5b80f6d3052a496b5fd68ff94bf0ea9a35b5
moumouwu <1137535547@qq.com>
2021-10-22 16:57:40 +0800

    commit

--- SpringBoot/Boot-Client/src/main/java/com/boot/client/controller/HelloController.java
+++ SpringBoot/Boot-Client/src/main/java/com/boot/client/controller/HelloController.java
@@ -47,6 +47,6 @@ public class HelloController {
 
         log.info("new了 100MB");
 
-        return "hi springboot addmin " + LocalDateTime.now();
+        return "hi springboot admin " + LocalDateTime.now();
     }
 }
